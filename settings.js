0132f07337363c814fa95827a4045af814a2d3e0
Vosty17 <stephwafula70@gmail.com>
2025-04-08 08:56:39 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,9 +3,9 @@ const settings = {
   author: 'Bot',
   botName: "Vosty",
   botOwner: 'Stephen', // Your name
-  ownerNumber: '+254762414548', //Your number
+  ownerNumber: '254762414548', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
