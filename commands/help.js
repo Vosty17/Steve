5031d284368265b50fe1aa88ca207751409de4da
Vosty17 <stephwafula70@gmail.com>
2025-04-11 12:16:33 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -129,12 +129,7 @@ Join our channel for updates:`;
             await sock.sendMessage(chatId, { 
                 text: helpMessage,
                 contextInfo: {
-                    forwardingScore: 999,
-                    isForwarded: true,
-                    forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363161513685998@newsletter',
-                        newsletterName: 'KnightBot MD by Mr Unique Hacker',
-                        serverMessageId: -1
+            
                     } 
                 }
             });
