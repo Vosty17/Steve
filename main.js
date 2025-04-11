0c3adb7a8fe918a87e3219b48709ccc34723bcb0
Vosty17 <stephwafula70@gmail.com>
2025-04-11 11:17:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -169,7 +169,7 @@ async function handleMessages(sock, messageUpdate, printLog) {
             isBotAdmin = adminStatus.isBotAdmin;
 
             if (!isBotAdmin) {
-                await sock.sendMessage(chatId, { text: 'Please make the bot an admin to use admin commands.', ...channelInfo });
+                await sock.sendMessage(chatId, { text: 'Please make the bot an admin to use admin commands.', });
                 return;
             }
 
