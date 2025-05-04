fd4918fec0681d72fbe160adecd35a5d2f3515de
Vosty17 <stephwafula70@gmail.com>
2025-05-04 23:35:53 +0300

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -12,15 +12,7 @@ _Star ⭐ the repository if you like the bot!_`;
     try {
         await sock.sendMessage(chatId, {
             text: repoInfo,
-            contextInfo: {
-                forwardingScore: 1,
-                isForwarded: true,
-                forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
-                    serverMessageId: -1
-                }
-            }
+            
         });
     } catch (error) {
         console.error('Error in github command:', error);
