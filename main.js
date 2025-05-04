d9dc8ec77c83d8eeb1bfd4ea0ff3d5f1fbf2f141
Vosty17 <stephwafula70@gmail.com>
2025-05-04 23:31:25 +0300

    Update main.js

--- main.js
+++ main.js
@@ -158,14 +158,14 @@ async function handleMessages(sock, messageUpdate, printLog) {
             return;
         }
 
-        /*  // Basic message response in private chat
+        // Basic message response in private chat
           if (!isGroup && (userMessage === 'hi' || userMessage === 'hello' || userMessage === 'bot' || userMessage === 'hlo' || userMessage === 'hey' || userMessage === 'bro')) {
               await sock.sendMessage(chatId, {
-                  text: 'Hi, How can I help you?\nYou can use .menu for more info and commands.',
+                  text: 'Hi, How can I help you?\n How are you?.',
                   
               });
               return;
-          } */
+          } 
 
         if (!message.key.fromMe) incrementMessageCount(chatId, senderId);
 
