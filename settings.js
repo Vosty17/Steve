89b1ad222c9f14115691b25c30339fb0c7e7177a
Vosty17 <stephwafula70@gmail.com>
2025-05-19 22:43:05 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Vosty-Bot",
   botOwner: 'Eng.Stephen', // Your name
-  ownerNumber: '254762414548', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254117812436', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
