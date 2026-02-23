4ef423871e4801f2ec6b53b724cb482822a32eff
Vosty17 <stephwafula70@gmail.com>
2026-02-23 14:02:23 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'VOSTY_BOT',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '254117812436', //Set your number here without + symbol, just add country code & number without any space
+  botName: "VOSTY_BOT",
+  botOwner: 'Stephen', // Your name
+  ownerNumber: '254743335229', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
